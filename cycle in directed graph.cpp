abd47b3020b0025a1c8760df377a9bb783f54aae
ShubhamKumar07 <49623544+ShubhamKumar07@users.noreply.github.com>
2019-07-06 18:18:42 +0530

    Add files via upload

---
+++ cycle in directed graph.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define INF 1000000007
+#define pb(x) push_back(x)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+typedef long long int ll;
+
+const ll N=2*100001;
+vector<ll>adj[N];
+bool vis[N];
+bool recstack[N];
+
+ll n,e;
+ll dfs(ll u)
+{
+    if(!vis[u])
+    {
+        vis[u]=1;
+        recstack[u]=1;
+        for(ll i=0;i<adj[u].size();i++)
+        {
+            ll v=adj[u][i];
+            if(!vis[v] && dfs(v))
+                return 1;
+            else if(recstack[v])
+            {
+
+               return 1;
+            }
+
+        }
+
+    }
+    recstack[u]=0;
+    return 0;
+}
+
+
+int main()
+{
+    hs;
+    ll t;
+    cin>>t;
+    f(t)
+    {
+    cin>>n>>e;
+    vector<ll>vx;
+    vector<ll>vy;
+    f(e)
+    {
+        ll x,y;
+        cin>>x>>y;
+        vx.pb(x);
+        vx.pb(y);
+        adj[x].pb(y);
+    }
+    ll ff=0;
+    r(e)
+    {
+    adj[vx[j]].remove(vy[j]);
+    mem(vis,0);
+    mem(recstack,0);
+
+    for(ll i=1;i<=n;i++)
+    {
+    if(!vis[i])
+    {
+        if(!dfs(i))
+        {
+            cout<<"YES"<<endl;
+            ff=1;
+            break;
+        }
+        else
+        {
+            adj[vx[j]].pb(vy[j]);
+        }
+    }
+    }
+    }
+    if(ff==0)
+        cout<<"NO"<<endl;
+
+
+
+    f(N)
+    {
+        adj[i].clear();
+    }
+    }
+    return 0;
+
+}
+
+/*
+https://www.spoj.com/problems/PT07Y/
+https://www.spoj.com/problems/EPIC1304/
+https://www.hackerearth.com/practice/algorithms/graphs/depth-first-search/practice-problems/algorithm/find-the-cycles/
+https://www.hackerrank.com/contests/101hack23/challenges/devu-and-cycles-of-a-graph
+https://www.codechef.com/problems/CHEFCYCL#
+https://www.codechef.com/TESS2018/problems/FOUR#
+https://codeforces.com/contest/915/problem/D
+https://codeforces.com/contest/937/problem/D
+https://codeforces.com/problemset/problem/263/D
+*/
+
