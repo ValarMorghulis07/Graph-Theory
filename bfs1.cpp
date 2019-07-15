129911305fed78746ce5ca9b38e5a630e6da95ab
Shubham Kumar <49623544+ShubhamKumar07@users.noreply.github.com>
2019-07-15 12:08:59 +0530

    Update bfs1.cpp

--- bfs1.cpp
+++ bfs1.cpp
@@ -1,103 +1,92 @@
-#include <bits/stdc++.h>
+ #include <bits/stdc++.h>
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define pb(x) push_back(x)
+#define m_p(x,y) make_pair(x,y)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define F first
+#define S second
+#define pi 3.14159265359
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
 using namespace std;
-typedef long long ll;
-#define mem(dp,a)           memset(dp,a,sizeof dp)
-#define rep(i,a,b)          for(ll i=a;i<b;i++)
-#define repb(i,a,b)         for(ll i=a;i>=b;i--)
-#define pb(x)               push_back(x)
-#define mp(x,y)             make_pair(x,y)
-#define tr(c,it)            for(auto const &it : c)
-#define xlr8                ios_base::sync_with_stdio(false);cin.tie(NULL)
-#define sz(c)               (c).size()
-#define all(c)              (c).begin(),(c).end()
-#define sl(a)               scanf("%lld",&a);
-#define TH                  1000
-#define F                   first
-#define S                   second
-ll MOD=pow(10,9)+7;
-ll INF=1e18+10;
+typedef long long int ll;
+ll HRX=1e18;
+ll INF=1e9+7;
 
+const ll N=2*100007;
+vector<ll>adj[N];
+bool vis[N];
+ll n,e;
 
-const int N=5000;
-vector <int> adj[N];
-
-
-void bfs(int a)
+void bfs(ll u)
 {
-
-    bool vis[N];
-    rep(i,0,N)
+  mem(vis,0);
+  ll dist[n+1];
+  f(n+1)
+  {
+    dist[i]=HRX;
+  }
+  dist[u]=0;
+  queue<ll>q;
+  if(!vis[u])
+    vis[u]=1;
+  q.push(u);
+  while(!q.empty())
+  {
+    ll u=q.front();
+    cout<<u<<" ";
+    q.pop();
+    f(adj[u].size())
     {
-        vis[i]=false;
+     ll v=adj[u][i];
+     if(!vis[v])
+     {
+       vis[v]=1;
+       dist[v]=dist[u]+1;
+       q.push(v);
+     }
     }
-    queue <int> q;
-    if(!vis[a])
-    vis[a]=true;
-    q.push(a);
+  }
 
-    while(!q.empty())
-    {
-        int b=q.front();
-        cout<<b<<" ";
-        q.pop();
-        for(auto it : adj[b])
-        {
-            if(!vis[it])
-            vis[it]=true,
-            q.push(it);
-        }
-
-
-    }
-    rep(i,0,5000)
-    {
-      adj[i].clear();
-    }
-
-
+  for(ll i=2;i<n+1;i++)
+  {
+    cout<<dist[i]<<" ";
+  }
+   f(N)
+  {
+    adj[i].clear();
+  }
 }
 
 int main()
 {
-    int t;
-    cin>>t;
-  for(int i=0;i<t;i++)
-    {
-    int m;
-    cin>>m; // number of edges assuming (number of nodes<=5000)
-    int directed;
-    cin>>directed;
-
-
-    if(directed)
-    {
-      rep(i,0,m)
-    {
-        int a,b;
-        cin>>a>>b;
-        adj[a].pb(b);
-
-    }
-
-    }
-    else
-    {
-    rep(i,0,m)
-    {
-        int a,b;
-        cin>>a>>b;
-        adj[a].pb(b);
-        adj[b].pb(a);
-    }
-    }
-
-    //considering 1 is always a node in the graph
-    int start;
-    cin>>start;
-    bfs(start);
-    }
-    return 0;
+  hs;
+  ll t;
+  cin>>t;
+  f(t)
+  {
+  cin>>n>>e;
+  f(e)
+  {
+    ll x,y;
+    cin>>x>>y;
+    adj[x].pb(y);
+    adj[y].pb(x);
+  }
+  ll src;
+  cin>>src;
+  bfs(src);
+  }
+  return 0;
 }
+
+
 /*
+https://www.hackerearth.com/practice/algorithms/graphs/breadth-first-search/practice-problems/algorithm/traffic-light-2-ee27ba45/
+https://www.codechef.com/problems/DECUNI
 https://codeforces.com/problemset/problem/59/E
+https://codeforces.com/problemset/problem/1037/D
+*/
 */
