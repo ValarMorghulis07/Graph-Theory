9fc59abd70b224b0bdadac6f9ff41b59b00774a9
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-07-24 02:22:23 +0530

    Update Replace O's with X's.cpp

--- Replace O's with X's.cpp
+++ Replace O's with X's.cpp
@@ -1,120 +1,62 @@
-#include <bits/stdc++.h>
-#define mem(dp,a) memset(dp,a,sizeof(dp))
-#define pb(x) push_back(x)
-#define m_p(x,y) make_pair(x,y)
-#define rep(i,a,b) for(ll i=a;i<b;i++)
-#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
-#define f(n) for(ll i=0;i<n;i++)
-#define r(n) for(ll j=0;j<n;j++)
-#define F first
-#define S second
-#define pi 3.14159265359
-#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
-using namespace std;
-typedef long long int ll;
-ll HRX=1e18;
-ll INF=1e9+7;
-
-const ll N=1e3+7;
-char ch[N][N];
+const int N=1e3+7;
 bool vis[N][N];
-ll dir[][2]={{0,1},{0,-1},{1,0},{-1,0}};
-ll m,n;
+int dir[][2]={{0,1},{0,-1},{1,0},{-1,0}};
+int m,n;
 
-void dfs1(ll x,ll y,char cc)
-{
- if(!vis[x][y])
- {
-  vis[x][y]=1;
-  ch[x][y]=cc;
-  for(ll i=0;i<4;i++)
-  {
-   ll xx=x+dir[i][0];
-   ll yy=y+dir[i][1];
-   if(xx>=0 && xx<m && yy>=0 && yy<n && !vis[xx][yy] && ch[xx][yy]=='O')
-   {
-    ch[xx][yy]=cc;
-    dfs1(xx,yy,cc);
-   }
-  }
- }
-}
 
-int main()
-{
- hs;
- ll t;
- cin>>t;
- f(t)
- {
-  cin>>m>>n;
-  mem(vis,0);
-  f(m)
-  {
-   r(n)
-   {
-    cin>>ch[i][j];
-   }
-  }
-  for(ll i=0;i<n;i++)
-  {
-   if(ch[0][i]=='O')
-    dfs1(0,i,'Y');
-  }
-  for(ll i=0;i<n;i++)
-  {
-   if(ch[m-1][i]=='O')
-    dfs1(m-1,i,'Y');
-  }
-  for(ll i=0;i<m;i++)
-  {
-   if(ch[i][0]=='O')
-    dfs1(i,0,'Y');
-  }
-  for(ll i=0;i<m;i++)
-  {
-   if(ch[i][n-1]=='O')
-    dfs1(i,n-1,'Y');
-  }
-  mem(vis,0);
-  f(m)
-  {
-   r(n)
-   {
-    if(ch[i][j]=='O')
-      dfs1(i,j,'X');
-   }
-  }
-  f(m)
-  {
-   r(n)
-   {
-    if(ch[i][j]=='Y')
-     cout<<"O"<<" ";
-    else
-     cout<<ch[i][j]<<" ";
-   }
-   cout<<"\n";
-  }
-
- }
- return 0;
-}
-
-/*
-3
-1 5
-X O X O X
-3 3
-X X X
-X O X
-X X X
-6 6
-X O X X X X
-X O X X O X
-X X X O O X
-O X X X X X
-X X X O X O
-O O X O O O
-*/
-
+class Solution {
+public:
+    
+    void dfs(vector<vector<char>>& board,int x,int y)
+    {
+     for(int i=0;i<4;i++)
+     {
+      int xx=x+dir[i][0];
+      int yy=y+dir[i][1];
+      if(xx>=0 && xx<m && yy>=0 && yy<n && !vis[xx][yy] && board[xx][yy]=='O')
+      {
+       vis[xx][yy]=1;
+       board[xx][yy]='#';
+       dfs(board,xx,yy);
+      }
+     }
+    }
+    void solve(vector<vector<char>>& board) 
+    {
+     m=board.size();
+     if(m==0)
+         return ;
+     n=board[0].size();
+     memset(vis,0,sizeof(vis));
+     vector<pair<int,int>>vv;
+     for(int i=0;i<m;i++)
+     {
+      for(int j=0;j<n;j++)
+      {
+       if((i==0 || j==0 || i==m-1 || j==n-1) && board[i][j]=='O')
+           vv.push_back(make_pair(i,j));
+      }
+     }
+     for(int i=0;i<vv.size();i++)
+     {
+      int x=vv[i].first;
+      int y=vv[i].second;
+      if(board[x][y]=='O')
+      {
+       board[x][y]='#';
+       dfs(board,x,y);
+      }
+     }
+     for(int i=0;i<m;i++)
+     {
+      for(int j=0;j<n;j++)
+      {
+       if(board[i][j]=='#')
+           board[i][j]='O';
+       else if(board[i][j]=='O')
+           board[i][j]='X';
+      }
+     }
+     
+    }
+};
