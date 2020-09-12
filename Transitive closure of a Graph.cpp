9198283c01b217e41453e2f42e1d78d96f0837df
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-09-12 13:10:37 +0530

    Update Transitive closure of a Graph.cpp

--- Transitive closure of a Graph.cpp
+++ Transitive closure of a Graph.cpp
@@ -85,3 +85,8 @@ int main()
  }
  return 0;
 }
+
+/*
+Transitive Closure it the reachability matrix to reach from vertex u to vertex v of a graph. One graph is given, we have to find a vertex v 
+which is reachable from another vertex u, for all vertex pairs (u, v). 
+*/
