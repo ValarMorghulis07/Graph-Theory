85f9b6aee28f2b000fded4ff03310efe60c65c44
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-10-04 17:53:45 +0530

    Update 79. Word Search.cpp

--- 79. Word Search.cpp
+++ 79. Word Search.cpp
@@ -3,29 +3,27 @@ int dir[][2]={{0,1},{0,-1},{1,0},{-1,0}};
 
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board,string word,int m,int n,int x,int y,int idx)
-    {
-     for(int i=0;i<4;i++)
+   bool dfs(vector<vector<char>>& board,string word,int m,int n,int x,int y,int idx)
+   {
+    for(int i=0;i<4;i++)
     {
     int xx=x+dir[i][0];
     int yy=y+dir[i][1];
-  if(idx==word.length())
-    return true;
-  if(xx>=0 && xx<m && yy>=0 && yy<n && idx<word.length() && board[xx][yy]==word[idx])
-  {
-   board[xx][yy]='$';//  this prevents reusage of characters
-   if(dfs(board,word,m,n,xx,yy,idx+1))
-   {
-    board[xx][yy]=word[idx];// again changing it to its previous state, if found
-     return true;
-   }
-   board[xx][yy]=word[idx];// again changing it to its previous state, if not found
-
-
-  }
- }
-  return false;
+    if(idx==word.length())
+      return true;
+    if(xx>=0 && xx<m && yy>=0 && yy<n && idx<word.length() && board[xx][yy]==word[idx])
+    {
+     board[xx][yy]='$';//  this prevents reusage of characters
+     if(dfs(board,word,m,n,xx,yy,idx+1))
+     {
+      board[xx][yy]=word[idx];// again changing it to its previous state, if found
+      return true;
+     }
+     board[xx][yy]=word[idx];// again changing it to its previous state, if not found
     }
+   }
+    return false;
+   }
     
     bool exist(vector<vector<char>>& board, string word) 
     {
