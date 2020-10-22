f03b7e175bc091165f26a2b8e13c5084af7a30dd
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-10-22 11:47:06 +0530

    Update graphcoloring.cpp

--- graphcoloring.cpp
+++ graphcoloring.cpp
@@ -1,3 +1,5 @@
+// graphcoloring
+
 #include <bits/stdc++.h>
 #define mem(dp,a) memset(dp,a,sizeof(dp))
 #define pb(x) push_back(x)
@@ -105,3 +107,127 @@ https://codeforces.com/problemset/problem/183/C
 https://www.hackerearth.com/practice/algorithms/graphs/depth-first-search/practice-problems/approximate/edges-coloridx9ng/
 https://code.google.com/codejam/contest/32010/dashboard#s=p2
 */
+
+
+// distinct coloring till distanc1
+
+#include <bits/stdc++.h>
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define pb(x) push_back(x)
+#define m_p(x,y) make_pair(x,y)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define F first
+#define S second
+#define pi 3.14159265359
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+typedef long long int ll;
+ll HRX=1e18;
+ll INF=1e9+7;
+
+const ll N=2*100007;
+vector<ll>adj[N];
+bool vis[N];
+ll n,k;
+ll ways[N];
+
+
+
+int main()
+{
+  hs;
+
+    cin>>n>>k;
+    f(n-1)
+    {
+     ll x,y;
+     cin>>x>>y;
+     adj[x].pb(y);
+     adj[y].pb(x);
+    }
+
+
+    ll way=1;
+    way=k;
+    for(ll i=2;i<=n;i++)
+    {
+      way=(way*(k-1))%INF;
+    }
+    cout<<way<<endl;
+
+
+  return 0;
+}
+
+
+// distinct coloring till distanc2
+
+#include <bits/stdc++.h>
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define pb(x) push_back(x)
+#define m_p(x,y) make_pair(x,y)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define F first
+#define S second
+#define pi 3.14159265359
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+typedef long long int ll;
+ll HRX=1e18;
+ll INF=1e9+7;
+
+const ll N=2*100007;
+vector<ll>adj[N];
+bool vis[N];
+ll n,k;
+ll ways[N];
+
+void dfs(ll u,ll level)
+{
+  vis[u]=1;
+  if(level==0)
+    ways[u]=k;
+  ll j=(level==0) ? 1:2;
+  for(ll i=0;i<adj[u].size();i++)
+  {
+    ll v=adj[u][i];
+    if(!vis[v])
+    {
+     ways[v]=k-j;
+     j++;
+     dfs(v,1);
+    }
+  }
+}
+
+int main()
+{
+  hs;
+
+    cin>>n>>k;
+    f(n-1)
+    {
+     ll x,y;
+     cin>>x>>y;
+     adj[x].pb(y);
+     adj[y].pb(x);
+    }
+
+    dfs(1,0);
+    ll way=1;
+    for(ll i=1;i<=n;i++)
+    {
+      way=(way*ways[i])%INF;
+    }
+    cout<<way<<endl;
+
+
+  return 0;
+}
+
