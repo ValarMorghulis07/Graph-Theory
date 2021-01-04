bfc5ab735a238ac64b1804a3f91fd6d7d98b73b8
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-01-04 14:09:23 +0530

    Update grid dfs & bfs.cpp

--- grid dfs & bfs.cpp
+++ grid dfs & bfs.cpp
@@ -255,3 +255,149 @@ public:
      
     }
 };
+
+
+// Knight Walk
+
+const int N=505;
+bool vis[N][N];
+int dir[][2]={{2,1},{2,-1},{-2,1},{-2,-1},{1,2},{1,-2},{-1,2},{-1,-2}};
+
+
+int Solution::knight(int A, int B, int C, int D, int E, int F) 
+{
+ int grid[A][B];
+ C--;D--;E--;F--;
+ if(E<0 || E>=500 || F<0 || F>=500)
+  return -1;
+ if(C==E && D==F)
+  return 0;
+ int dist[A][B];
+ memset(vis,0,sizeof(vis));
+ int sx=C,sy=D,dx=E,dy=F;
+ for(int i=0;i<A;i++)
+ {
+  for(int j=0;j<B;j++)
+  {
+   dist[i][j]=INT_MAX; 
+   
+  }
+ }
+ dist[sx][sy]=0;
+ queue<pair<int,int>>q;
+ q.push(make_pair(sx,sy));
+ vis[sx][sy]=1;
+ while(!q.empty())
+ {
+  pair temp=q.front();
+  q.pop();
+  int x=temp.first;
+  int y=temp.second;
+  for(int i=0;i<8;i++)
+  {
+   int xx=x+dir[i][0];
+   int yy=y+dir[i][1];
+   if(xx>=0 && xx<A && yy>=0 && yy<B && !vis[xx][yy])
+   {
+     vis[xx][yy]=1;
+     dist[xx][yy]=1+dist[x][y];
+     q.push(make_pair(xx,yy));
+   }
+  }
+ }
+ 
+ /*for(int i=0;i<A;i++)
+ {
+  for(int j=0;j<B;j++)
+  {
+    cout<<dist[i][j]<<" ";
+  }
+  cout<<"\n";
+ }*/
+ if(dist[dx][dy]!=INT_MAX)
+  return dist[dx][dy];
+ else
+  return -1;
+ 
+}
+
+
+// Minimum Cost Path
+
+
+#include<bits/stdc++.h>
+#define mem(dp,a) memset(dp,a,sizeof(dp))
+#define pb(x) push_back(x)
+#define m_p(x,y) make_pair(x,y)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define repush_back(i,a,b) for(ll i=a;i>=b;i--)
+#define f(n) for(ll i=0;i<n;i++)
+#define r(n) for(ll j=0;j<n;j++)
+#define F first
+#define S second
+#define pi 3.14159265359
+#define hs ios_base::sync_with_stdio(false);cin.tie(NULL);
+using namespace std;
+typedef long long int ll;
+typedef pair<ll,pair<ll,ll>>PI;
+ll HRX=1e18;
+ll INF=1e9+7;
+
+
+
+ll dir[][2]={{1,0},{-1,0},{0,1},{0,-1}};
+
+
+int main()
+{
+ hs;
+ ll t;
+ cin>>t;
+ f(t)
+ {
+  ll n;
+  cin>>n;
+  ll a[n][n];
+  ll dist[n][n];
+  bool vis[n][n];
+  f(n)
+  {
+    r(n)
+    {
+     cin>>a[i][j];
+     vis[i][j]=0;
+     dist[i][j]=HRX;
+    }
+  }
+  priority_queue<PI,vector<PI>,greater<PI>>pq;
+  pq.push(m_p(a[0][0],m_p(0,0)));
+  dist[0][0]=a[0][0];
+  vis[0][0]=1;
+
+  while(!pq.empty())
+  {
+   PI u=pq.top();
+   pq.pop();
+   ll weight=u.first;
+   ll x=u.second.first;
+   ll y=u.second.second;
+   /*if(vis[x][y])
+     continue;
+   vis[x][y]=1;*/
+   for(ll i=0;i<4;i++)
+   {
+    ll xx=x+dir[i][0];
+    ll yy=y+dir[i][1];
+    if(xx>=0 && yy>=0 && xx<n && yy<n && dist[xx][yy]>dist[x][y]+a[xx][yy] && !vis[xx][yy])
+    {
+     vis[xx][yy]=1;
+     dist[xx][yy]=dist[x][y]+a[xx][yy];
+     pq.push(m_p(dist[xx][yy],m_p(xx,yy)));
+    }
+   }
+  }
+  cout<<dist[n-1][n-1]<<endl;
+}
+ return 0;
+}
+
