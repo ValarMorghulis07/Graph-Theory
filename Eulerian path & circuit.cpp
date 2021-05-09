7748807a8c1ab9c07df1a1bbd49aed4b6f49dd7a
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-05-09 14:22:18 +0530

    Update Eulerian path & circuit.cpp

--- Eulerian path & circuit.cpp
+++ Eulerian path & circuit.cpp
@@ -1,3 +1,5 @@
+// Eulerian Path is a path in graph that visits every edge exactly once. Eulerian Circuit is an Eulerian Path which starts and ends on the same vertex.
+
 // Eulerian path in Directed Graph
 
 /*
